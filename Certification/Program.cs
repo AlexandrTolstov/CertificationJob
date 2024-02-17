e0c2f8761ffa7a5fc3f90eb129c9643d6266473e
Alexandr Tolstov <tolstov.alexandr.85@gmail.com>
2024-02-17 17:57:04 +0600

    Ввод с консоли строк

--- Certification/Program.cs
+++ Certification/Program.cs
@@ -2,3 +2,10 @@
 Console.Write("NumOfString = ");
 int NumOfString = Convert.ToInt32(Console.ReadLine());
 string[] Strings = new string[NumOfString];
+
+Console.WriteLine("Введите строки массива:");
+for (int i = 0; i < Strings.Length; i++)
+{
+    Console.Write($"Str{i+1} = ");
+    Strings[i] = Console.ReadLine();
+}
