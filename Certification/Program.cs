0acd38a37a693fbf7c21fc697eafbeae7b7a4d80
Alexandr Tolstov <tolstov.alexandr.85@gmail.com>
2024-02-17 17:56:31 +0600

    Ввод количества элементов для массива строк

--- Certification/Program.cs
+++ Certification/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Введите количество строк в массиве:");
+Console.Write("NumOfString = ");
+int NumOfString = Convert.ToInt32(Console.ReadLine());
+string[] Strings = new string[NumOfString];
